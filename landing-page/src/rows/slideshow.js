9fcdc3ae7a841590d7d6345f42ba2310a3ddb016
Dylan <dylanabozarth@gmail.com>
2022-11-06 14:11:07 -0800

    images added

--- landing-page/src/rows/slideshow.js
+++ landing-page/src/rows/slideshow.js
@@ -3,9 +3,9 @@ import './slideshow.scss'
 export const slideData = [
     {
       index: 0,
-      headline: 'New Fashion Apparel',
-      button: 'Shop now',
-      src: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/225363/fashion.jpg'
+      headline: 'Command your fleet',
+      button: 'More information',
+      src: './ships.webp'
     },
     {
       index: 1,
