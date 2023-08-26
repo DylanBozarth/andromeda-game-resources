8854acb50138568408efccc1d4078f46c7011000
Dylan <dylanabozarth@gmail.com>
2023-08-26 13:42:46 -0700

    lore

---
+++ lore/backstory.js
@@ -0,0 +1,7 @@
+/*
+After losing an interstellar war in the milky way galaxy, humanity has been pushed through a wormhole into the nearby galaxy of andromeda by the victors. 
+The wormhole was opened by The Caretakers, an ancient race that seeks to preserve all intelligent life in the galaxy.
+Now humanity has to restart in a brand new galaxy.
+
+
+*/
