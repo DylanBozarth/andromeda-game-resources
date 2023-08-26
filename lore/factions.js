35d2edb3d164bf24171887210689a157efe11919
Dylan <dylanabozarth@gmail.com>
2023-08-26 13:49:56 -0700

    factions

--- lore/factions.js
+++ lore/factions.js
@@ -1,4 +1,23 @@
 /*
+The Templars
+Exists as a banking clan with the set dressing as a religious faction. Loans money, cares a little bit about morals but not that much.
 
+Princess Andromeda
+Extremely rich older woman who is essentially cosplaying as royalty. Cares about her ego and willing to serve her or not. 
+
+The Colonial Republic
+Bleeding hearts types who want the best for everyone. Homesteaders who appreciate freedom and the taking of refugees.
+
+The Circle of life
+Big hippie scientists who want to collect and catalog every species of life in the galaxy. Likes nature reserves and science stuff, hates industrialism 
+
+New Dawn
+Anti-slavery freedom fighters faction. Known for being really good at fighting and spying, hates slavery, hates authoritarianism.
+
+Caretakers
+Generally stays out of human affairs. Ancient race that does not care about human societies just about the species as a whole.
+
+Wai-Xing
+Mysterious beings who live in the empty space between stars. Nothing is known about them except they attack on sight and are extremely deadly. 
 
 */
--- lore/technology.js
+++ lore/technology.js
@@ -1,3 +1,8 @@
 /*
-
+Space is very big, faster than light travel does not exist. 
+There is very fast (relatively) interstellar travel but there are limitations
+1. It is a one shot trip. There is no stopping unless you apply an equal force to the other direction. You can't just turn the engines off and visit a nearby planet, an emergency stop would leave you stranded as you cannot carry that much fuel.
+2. Because of the long travel times between stars, isolated planets and stars are very rarely governed by offworld governments. Similar to colonies in Earth's history. Things would simply take too long to govern.
+3. Hypersleep is used to travel between stars, and general relativity means that the time spent on board is not the same as the travel time. (this wont affect the game it will only affect lore)
+4. Large parts of the galaxy remain unexplored because of the speed limitation. 
 */
